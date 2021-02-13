e81b4d0c14497afe00bd0ec90c5a99e6118b2cc1
kms20a <kms21@naver.com>
2021-02-13 12:32:24 +0900

    useEffect 함수 예제

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Third from './aa/Third';
 import './App.css';
+import Sub from './Sub';
+import { num } from './Sub';
 
+//map, filter, concat, spread, slice
 function App() {
-  //다운로드 받음
-  const [users, setUsers] = useState([]);
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
   const download = () => {
-    let sample = [
-      { id: 1, name: '홍길동' },
-      { id: 2, name: '임꺽정' },
-      { id: 3, name: '장보고' },
-      { id: 4, name: '코스' },
-    ];
-    // setUsers([...sample]);
-    setUsers(sample);
+    //다운로드 받고(통신)
+    let downloadData = 5; //가정
+    setData(downloadData);
   };
 
-  //렌더링 시점 = 상태값 변경
+  //실행시점 :
+  //(1) App() 함수가 최초 실행될 때(그림이 그려질 때)
+  //(2) 상태 변수가 변경될 때(그게 dependencyList에 등록되어 있어야 함)
+  useEffect(() => {
+    console.log('useEffect 실행됨');
+    download();
+  }, []);
+
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id}, {u.name}
-        </h1>
-      ))}
+      <Sub />
+      <Third />
+      Hello World1
+      {num}
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색하기
+      </button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
