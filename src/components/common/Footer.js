daba1abf58398cc79b40c55bdd700a7421c67ddb
kms20a <kms21@naver.com>
2021-02-14 15:41:27 +0900

    component-style 명명규칙(Styled+기능+엘리먼트)

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 //하나의 컴포넌트를 생성(재사용)
 
 //styled-components => js파일과 css파일 관리
-const FooterList = styled.div`
+const StyledFooterDiv = styled.div`
   border: 1px solid black;
   height: 300px;
 `;
 
 const Footer = () => {
   return (
-    <FooterList>
+    <StyledFooterDiv>
       <ul>
         <li>오시는 길: 서울 강남구.. </li>
         <li>전화번호: 01200000</li>
       </ul>
-    </FooterList>
+    </StyledFooterDiv>
   );
 };
 
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 //하나의 컴포넌트를 생성(재사용)
 
 //styled-components => js파일과 css파일 관리
-const HeaderList = styled.div`
+const StyledHeaderDiv = styled.div`
   border: 1px solid black;
   height: 300px;
 `;
 
 const Header = () => {
   return (
-    <HeaderList>
+    <StyledHeaderDiv>
       <ul>
         <li>메뉴 1</li>
         <li>메뉴 2</li>
       </ul>
-    </HeaderList>
+    </StyledHeaderDiv>
   );
 };
 
